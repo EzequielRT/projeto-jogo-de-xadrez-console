aa859bd03b32c4e3b994be5efbf276fe37bf3284
EzequielRT <ezefuts@gmail.com>
2021-09-04 23:27:12 -0300

    Comecando a implementar o tabuleiro

---
+++ tabuleiro/Cor.cs
@@ -0,0 +1,13 @@
+﻿namespace tabuleiro
+{
+    enum Cor : int
+    {
+        Branca = 0,
+        Preta = 1,
+        Verde = 2,
+        Amarela = 3,
+        Azul = 4,
+        Vermelha = 5,
+        Laranja = 6
+    }
+}
